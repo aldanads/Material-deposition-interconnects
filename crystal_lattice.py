Bug: Thickness calculation

crystal_lattice.py
@@ -662,8 +662,11 @@ class Crystal_Lattice():
     def RMS_roughness(self):
         
         x,y,z = self.obtain_surface_coord()
+        z = np.array(z)
         z_mean = np.mean(z)
-        self.surf_roughness_RMS = np.sqrt(np.mean((np.array(z)-z_mean)**2))
+        self.Ra_roughness = sum(abs(z-z_mean))/len(z)
+        self.z_mean = z_mean
+        self.surf_roughness_RMS = np.sqrt(np.mean((z-z_mean)**2))
     
     def islands_analysis(self):
 
@@ -687,16 +690,73 @@ class Crystal_Lattice():
                     total_visited,island_slice = self.detect_islands(idx_site,total_visited,island_slice,self.chemical_specie)
 
                     if len(island_slice):
-                        island_visited = set()
+                        island_visited = island_slice.copy()
                         island_sites = island_slice.copy()
                         island_visited,island_sites = self.build_island(island_visited,island_sites,list(island_slice)[0],self.chemical_specie)
                         islands_list.append(Island(z_idx,z_layer,island_sites))
                         count_islands[z_idx] += 1
                         total_visited.update(island_visited)
 
+
                         
         self.islands_list = islands_list
         
+# =============================================================================
+#     Function to detect island and the coordinates of the base
+# =============================================================================
+    def detect_islands(self,idx_site,visited,island_slice,chemical_specie):
+
+        site = self.grid_crystal[idx_site] 
+        
+        if idx_site not in visited and site.chemical_specie == chemical_specie:
+            visited.add(idx_site)
+            island_slice.add(idx_site)
+            # dfs_recursive
+            for idx in site.migration_paths['Plane']:
+                if idx[0] not in visited:
+                    visited,island_slice = self.detect_islands(idx[0],visited,island_slice,chemical_specie)
+                                       
+        return visited,island_slice
+
+# =============================================================================
+#     Function to build the full island starting from the base obtained in detect_islands()
+# =============================================================================
+    def build_island_2(self,visited,island_sites,idx,chemical_specie):
+          
+        site = self.grid_crystal[idx]
+            
+        for element in site.migration_paths['Up'] + site.migration_paths['Plane']+site.migration_paths['Down']:
+    
+            if element[0] not in visited and self.grid_crystal[element[0]].chemical_specie == chemical_specie:
+                visited.add(element[0])
+                island_sites.add(element[0])
+                visited,island_sites = self.build_island(visited,island_sites,element[0],chemical_specie)
+                
+        return visited,island_sites
+    
+    def build_island(self,visited,island_sites,start_idx,chemical_specie):
+          
+        stack = [start_idx]
+
+        while stack:
+            idx = stack.pop()
+            site = self.grid_crystal[idx]
+            
+            for element in site.migration_paths['Up'] + site.migration_paths['Plane'] + site.migration_paths['Down']:
+        
+                if element[0] not in visited and self.grid_crystal[element[0]].chemical_specie == chemical_specie:
+                    visited.add(element[0])
+                    island_sites.add(element[0])
+                    stack.append(element[0])
+
+        return visited,island_sites
+    
+    
+
+
+
+
+
         
 # =============================================================================
 #     Auxiliary functions
@@ -809,43 +869,13 @@ class Crystal_Lattice():
     def idx_to_cart(self,idx):
         return tuple(round(element,3) for element in np.sum(idx * np.transpose(self.basis_vectors), axis=1))
     
-    
-# =============================================================================
-#     Function to detect island and the coordinates of the base
-# =============================================================================
-    def detect_islands(self,idx_site,visited,island_slice,chemical_specie):
 
-        site = self.grid_crystal[idx_site] 
-        
-        if idx_site not in visited and site.chemical_specie == chemical_specie:
-            visited.add(idx_site)
-            island_slice.add(idx_site)
-            # dfs_recursive
-            for idx in site.migration_paths['Plane']:
-                visited,island_slice = self.detect_islands(idx[0],visited,island_slice,chemical_specie)
-                                       
-        return visited,island_slice
-
-# =============================================================================
-#     Function to build the full island starting from the base obtained in detect_islands()
-# =============================================================================
-    def build_island(self,visited,island_sites,idx,chemical_specie):
-          
-        site = self.grid_crystal[idx]
-            
-        for element in site.migration_paths['Up'] + site.migration_paths['Plane']:
-    
-            if element[0] not in visited and self.grid_crystal[element[0]].chemical_specie == chemical_specie:
-                visited.add(element[0])
-                island_sites.add(element[0])
-                visited,island_sites = self.build_island(visited,island_sites,element[0],chemical_specie)
-                
-        return visited,island_sites
     
     def obtain_surface_coord(self):
         
         grid_crystal = self.grid_crystal
-        
+        z_step = self.basis_vectors[0][2]
+
         x = []
         y = []
         z = []
@@ -856,6 +886,11 @@ class Crystal_Lattice():
                 if grid_crystal[jump[0]].chemical_specie == 'Empty': top_layer_empty_sites +=1
                      
             if (site.chemical_specie != 'Empty') and top_layer_empty_sites >= 2:
+                x.append(site.position[0])
+                y.append(site.position[1])
+                z.append(site.position[2]+z_step)
+                
+            elif (site.chemical_specie == 'Empty') and ('Substrate' in site.supp_by) and top_layer_empty_sites >= 2:
                 x.append(site.position[0])
                 y.append(site.position[1])
                 z.append(site.position[2])

extract_data.py
@@ -17,19 +17,19 @@ class SimulationResults:
         self.excel_filename = excel_filename
         # Initialize a CSV file with headers
         with open(excel_filename, 'w') as f:
-            f.write('Substrate,Partial_pressure,Temperature,Thickness,RMS_roughness,n_islands,mean_size_islands,std_size_islands,max_size_island,mean_island_terraces,std_island_terraces,max_island_terrace,general_terrace\n')
+            f.write('Substrate,Partial_pressure,Temperature,Thickness,Ra_roughness,z_mean,RMS_roughness,n_islands,mean_size_islands,std_size_islands,max_size_island,mean_island_terraces,std_island_terraces,max_island_terrace,general_terrace\n')
     
-    def measurements_crystal(self, Substrate, folder_P,temperature,thickness,roughness,n_islands,mean_size_islands,std_size_islands,max_size_island,mean_island_terraces,std_island_terraces,max_island_terrace,general_terrace):
+    def measurements_crystal(self, Substrate, folder_P,temperature,thickness,Ra_roughness,z_mean,roughness,n_islands,mean_size_islands,std_size_islands,max_size_island,mean_island_terraces,std_island_terraces,max_island_terrace,general_terrace):
             # Append measurements to the CSV file
             with open(self.excel_filename, 'a') as f:
-                f.write(f'{Substrate},{folder_P},{temperature},{thickness},{roughness},{n_islands},{mean_size_islands},{std_size_islands},{max_size_island},{mean_island_terraces},{std_island_terraces},{max_island_terrace},{general_terrace}\n')
+                f.write(f'{Substrate},{folder_P},{temperature},{thickness},{Ra_roughness},{z_mean},{roughness},{n_islands},{mean_size_islands},{std_size_islands},{max_size_island},{mean_island_terraces},{std_island_terraces},{max_island_terrace},{general_terrace}\n')
 
 
 
 
 # Path to the variables files, for every folder and Sim_i
-path = r'\\FS1\Docs2\samuel.delgado\My Documents\Publications\Copper deposition\Simulations\Batch simulations\Thickness limit\\'
-folder_P = '\P=0.1'
+path = r'\\FS1\Docs2\samuel.delgado\My Documents\Publications\Copper deposition\Simulations\Batch simulations\step_ascent\\'
+#folder_P = '\P=0.1'
 folder_subs = os.listdir(path)
 path_2 = r'\Program\\'
 
@@ -52,59 +52,61 @@ dfs_occ_rate = []
 
 for subs in folder_subs:
     
-    folder_1 = os.listdir(path+subs+folder_P)
+    for folder_P in os.listdir(path+subs):
     
-    for folder in folder_1:
-
-        if choose_system == 'Windows':
-            import shelve
-            
-            filename = path + subs+ folder_P + r'\\' + folder + path_2 + 'variables'
-
-            my_shelf = shelve.open(filename)
-            for key in my_shelf:
-                globals()[key]=my_shelf[key]
-            my_shelf.close()
-            
-        elif choose_system == 'Linux':
-            
-            import pickle
-            filename = path + subs+ folder_P + r'\\' + folder + path_2 + 'variables.pkl'
-            print(filename)
-            
-            # Open the file in binary mode
-            with open(filename, 'rb') as file:
-              
-                # Call load method to deserialze
-                myvar = pickle.load(file)
+        folder_1 = os.listdir(path+subs + r'\\' + folder_P)
+        
+        for folder in folder_1:
+    
+            if choose_system == 'Windows':
+                import shelve
                 
-            Co_latt = myvar['Co_latt']
-            
-            Co_latt.islands_analysis()
-            island_size = []
-            for island in Co_latt.islands_list:
-                island_size.append(len(island.island_sites))
+                filename = path + subs + r'\\' + folder_P + r'\\' + folder + path_2 + 'variables'
+    
+                my_shelf = shelve.open(filename)
+                for key in my_shelf:
+                    globals()[key]=my_shelf[key]
+                my_shelf.close()
                 
-            island_mean_size = np.mean(island_size)
-            island_std_size = np.std(island_size)
-            
-            islands_terraces = []
-            for island in Co_latt.islands_list:
-                island.layers_calculation(Co_latt)
-                islands_terraces.append(np.mean(np.array(island.terraces)[np.array(island.terraces) != 0]))
-            
-            # Results: thickness, roughness, islands, terraces
-            Results.measurements_crystal(subs,folder_P[1:],temperature[folder],Co_latt.thickness,Co_latt.surf_roughness_RMS,
-                                         len(Co_latt.islands_list),np.mean(island_size),np.std(island_size),max(island_size)
-                                         ,np.mean(islands_terraces),np.std(islands_terraces),max(islands_terraces),np.mean(np.array(Co_latt.terraces)[np.array(Co_latt.terraces) != 0]))
-            
-            # Size of islands in a list
-            df_histogram = pd.DataFrame({subs + '_' + str(temperature[folder]) + '_island_size': island_size})   
-            dfs_histogram.append(df_histogram)
-            
-            # Ocuppation rate per layer
-            df_occ_rate = pd.DataFrame({subs + '_' + str(temperature[folder]) + '_occupation_rate': Co_latt.layers[1]})   
-            dfs_occ_rate.append(df_occ_rate)
+            elif choose_system == 'Linux':
+                
+                import pickle
+                filename = path + subs + r'\\' + folder_P + r'\\' + folder + path_2 + 'variables.pkl'
+                
+                # Open the file in binary mode
+                with open(filename, 'rb') as file:
+                    print(filename)
+                    # Call load method to deserialze
+                    myvar = pickle.load(file)
+                    
+                Co_latt = myvar['Co_latt']
+                Co_latt.islands_analysis()
+                Co_latt.RMS_roughness()
+
+                island_size = []
+                for island in Co_latt.islands_list:
+                    island_size.append(len(island.island_sites))
+                    
+                island_mean_size = np.mean(island_size)
+                island_std_size = np.std(island_size)
+                islands_terraces = []
+                for island in Co_latt.islands_list:
+                    island.layers_calculation(Co_latt)
+                    islands_terraces.append(np.mean(np.array(island.terraces)[np.array(island.terraces) != 0]))
+                
+                # Results: thickness, roughness, islands, terraces
+                Results.measurements_crystal(subs,folder_P[2:],temperature[folder],Co_latt.thickness,Co_latt.Ra_roughness,Co_latt.z_mean,Co_latt.surf_roughness_RMS,
+                                             len(Co_latt.islands_list),np.mean(island_size),np.std(island_size),max(island_size)
+                                             ,np.mean(islands_terraces),np.std(islands_terraces),max(islands_terraces),np.mean(np.array(Co_latt.terraces)[np.array(Co_latt.terraces) != 0]))
+                
+                # Size of islands in a list
+                df_histogram = pd.DataFrame({subs + '_' + folder_P + '_' + str(temperature[folder]) + '_island_size': island_size})   
+                dfs_histogram.append(df_histogram)
+                
+                # Ocuppation rate per layer
+                df_occ_rate = pd.DataFrame({subs + '_' + folder_P + '_' + str(temperature[folder]) + '_occupation_rate': Co_latt.layers[1]})   
+                dfs_occ_rate.append(df_occ_rate)
+                
 
 df_combined = pd.concat(dfs_histogram, axis=1)    
 df_combined.to_csv(name_histogram_size_file, index=False)