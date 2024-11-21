Bug: Recover original state grid_crystal after superbasin calculation

KMC.py
@@ -23,14 +23,17 @@ def KMC(System_state,rng):
     TR_catalog = []
 
     for idx in System_state.sites_occupied + System_state.adsorption_sites:
+        
         if idx not in superbasin_dict:
             TR_catalog.extend([(item[0],item[1],item[2],idx) for item in grid_crystal[idx].site_events])
         else:
             TR_catalog.extend([(item[0],item[1],item[2],idx) for item in superbasin_dict[idx].site_events_absorbing])
 
-    
+
     # Sort the list of events
     sorted(TR_catalog,key = lambda x:x[0])
+    
+    
     # Build a balanced tree structure
     TR_tree = build_tree(TR_catalog)
     # Each node is the sum of their children, starting from the leaf
@@ -42,6 +45,11 @@ def KMC(System_state,rng):
     if type(sumTR) is tuple: sumTR = sumTR[0]
     # We search in our binary tree the event that happen
     chosen_event = search_value(TR_tree,sumTR*rng.random())
+    
+    
+    if chosen_event[-1] == (7,-4,11):
+            print('Chosen event:',chosen_event)
+            
     #Calculate the time step
     time_step += -np.log(rng.random())/sumTR
     # If the time step is big because of the TR, we need to allow the deposition process to occur

initialization.py
@@ -17,9 +17,9 @@ import json
 
 def initialization(n_sim,save_data):
     
-    # seed = 1
+    seed = 1
     # Random seed as time
-    rng = np.random.default_rng() # Random Number Generator (RNG) object
+    rng = np.random.default_rng(seed) # Random Number Generator (RNG) object
 
     # Default resolution for figures
     plt.rcParams["figure.dpi"] = 100 # Default value of dpi = 300
@@ -28,7 +28,7 @@ def initialization(n_sim,save_data):
         files_copy = ['initialization.py', 'crystal_lattice.py','Site.py','main.py','KMC.py','balanced_tree.py','analysis.py']
         
         if platform.system() == 'Windows': # When running in laptop
-            dst = r'\\FS1\Docs2\samuel.delgado\My Documents\Publications\Copper deposition\Simulations\Test\\'
+            dst = r'\\FS1\Docs2\samuel.delgado\My Documents\Publications\Material deposition exploration\Simulations\Test\\'
         elif platform.system() == 'Linux': # HPC works on Linux
             dst = r'/sfiwork/samuel.delgado/Copper_deposition/Varying_substrate/batch_simulation/annealing/TaN/T300/'
             
@@ -38,7 +38,7 @@ def initialization(n_sim,save_data):
         paths = {'data': ''}
         Results = []
         
-    ovito_file = False
+    ovito_file = True
 
     experiments = ['deposition','annealing']
     experiment = experiments[0]
@@ -245,12 +245,28 @@ def search_superbasin(System_state):
     
     # This approach should be more efficient and memory-friendly
     sites_occupied = System_state.sites_occupied[:] 
+    
+    # print('Before calling superbasin')
+    # for site in sites_occupied:
+    #     print(site,System_state.grid_crystal[site].chemical_specie)
+    #     if System_state.grid_crystal[site].chemical_specie == 'Empty':
+    #         print(sites_occupied)
+    #         quit()
 
     for idx in sites_occupied:
         for event in System_state.grid_crystal[idx].site_events:
             if (idx not in System_state.superbasin_dict) and (event[3] <= System_state.E_min):
-                System_state.superbasin_dict.update({idx: Superbasin(idx, System_state, System_state.E_min)})
+                System_state.superbasin_dict.update({idx: Superbasin(idx, System_state, System_state.E_min,sites_occupied)})
                 
+                
+    # print('After calling superbasin')
+    # for site in sites_occupied:
+    #     print(site,System_state.grid_crystal[site].chemical_specie)
+    #     if System_state.grid_crystal[site].chemical_specie == 'Empty':
+    #         print(sites_occupied)
+    #         print(System_state.sites_occupied)
+    #         quit()
+
 
 def save_simulation(files_copy,dst,n_sim):
     

main.py
@@ -9,27 +9,27 @@ Created on Mon Jan 15 15:12:23 2024
 """
 Profiling - Reference: Grown 0.4 nm in a 2x2nm box
 
-         2676429707 function calls (2668330686 primitive calls) in 9996.511 seconds
+3588847061 function calls (3583118442 primitive calls) in 4531.376 seconds
 
    Ordered by: internal time
-   List reduced from 12819 to 15 due to restriction <15>
+   List reduced from 3094 to 15 due to restriction <15>
 
    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
- 87014705 1697.435    0.000 1993.252    0.000 crystal_lattice.py:321(available_adsorption_sites)
- 98945669  966.934    0.000 1526.271    0.000 Site.py:160(available_migrations)
- 87014705  785.962    0.000 1871.621    0.000 Site.py:331(detect_planes)
- 98945669  760.480    0.000  915.542    0.000 Site.py:418(transition_rates)
- 87014705  746.771    0.000 3516.502    0.000 Site.py:95(supported_by)
- 87014705  527.334    0.000  700.369    0.000 Site.py:380(detect_edges)
--1821479170/-1821489333  440.519   -0.000  440.525   -0.000 {built-in method builtins.len}
-  4458920  400.697    0.000 8351.543    0.002 crystal_lattice.py:658(update_sites)
-326221503  400.266    0.000  400.266    0.000 Site.py:336(<lambda>)
- 88581059  367.008    0.000  767.598    0.000 {built-in method builtins.sorted}
-397918711  297.828    0.000  375.677    0.000 Site.py:115(calculate_clustering_energy)
- 93885924  279.128    0.000  279.166    0.000 {built-in method numpy.array}
-    13224  215.039    0.016  474.352    0.036 superbasin.py:107(transition_matrix)
-619315526  213.524    0.000  213.545    0.000 {built-in method builtins.max}
-  4458667  180.502    0.000  284.071    0.000 crystal_lattice.py:748(remove_specie_site)
+ 82083948  963.713    0.000 1404.471    0.000 Site.py:208(available_migrations)
+ 74649433  416.469    0.000 1025.775    0.000 Site.py:89(supported_by)
+ 82083948  289.458    0.000  426.047    0.000 Site.py:475(transition_rates)
+328902104  278.530    0.000  278.578    0.000 Site.py:139(calculate_clustering_energy)
+ 81337137  215.623    0.000  215.644    0.000 {built-in method numpy.array}
+  3820373  192.869    0.000 3213.572    0.001 crystal_lattice.py:659(update_sites)
+    12054  188.685    0.016  408.147    0.034 superbasin.py:107(transition_matrix)
+514541761  176.782    0.000  176.787    0.000 {built-in method builtins.max}
+576148287  149.388    0.000  149.388    0.000 {method 'append' of 'list' objects}
+ 74649433  148.408    0.000  404.024    0.000 Site.py:379(detect_planes)
+  3820122  146.402    0.000  230.872    0.000 crystal_lattice.py:748(remove_specie_site)
+599989208/599983845  122.053    0.000  122.056    0.000 {built-in method builtins.len}
+    12054  115.540    0.010 3761.155    0.312 superbasin.py:43(trans_absorbing_states)
+  3820373  115.057    0.000  164.150    0.000 crystal_lattice.py:322(available_adsorption_sites)
+128842061  112.088    0.000  184.653    0.000 {method 'update' of 'set' objects}
 
          9034848122 function calls (9028225809 primitive calls) in 13273.455 seconds
 
@@ -59,113 +59,113 @@ Profiling - Reference: Grown 0.4 nm in a 2x2nm box
 import cProfile
 import sys
 
-def main():
-    from initialization import initialization,save_variables,search_superbasin
-    from KMC import KMC
-    import numpy as np
-    import time
+# def main():
+from initialization import initialization,save_variables,search_superbasin
+from KMC import KMC
+import numpy as np
+import time
+
+save_data = True
+
+for n_sim in range(0,1):
     
-    save_data = False
+
+    System_state,rng,paths,Results = initialization(n_sim,save_data)
+    System_state.add_time()
+
+    System_state.plot_crystal(45,45,paths['data'],0)    
+    j = 0
     
-    for n_sim in range(0,1):
+    snapshoots_steps = int(2e1)
+    starting_time = time.time()
+# =============================================================================
+#     Deposition
+# 
+# =============================================================================
+    if System_state.experiment == 'deposition':   
         
-    
-        System_state,rng,paths,Results = initialization(n_sim,save_data)
-        System_state.add_time()
-    
-        System_state.plot_crystal(45,45)    
-        j = 0
-        
-        snapshoots_steps = int(2e1)
-        starting_time = time.time()
-    # =============================================================================
-    #     Deposition
-    # 
-    # =============================================================================
-        if System_state.experiment == 'deposition':   
+        nothing_happen = 0
+        list_time_step = []
+        thickness_limit = 4 # (1 nm)
+        System_state.measurements_crystal()
+        i = 0
+        while System_state.thickness < thickness_limit:
+            i+=1
+            System_state,KMC_time_step = KMC(System_state,rng)
+            # quit()
+            list_time_step.append(KMC_time_step)
+            if np.mean(list_time_step[-System_state.n_search_superbasin:]) <= System_state.time_step_limits:
+                nothing_happen +=1    
+            else:
+                nothing_happen = 0
             
-            nothing_happen = 0
-            list_time_step = []
-            thickness_limit = 4 # (1 nm)
-            System_state.measurements_crystal()
-            i = 0
-            while System_state.thickness < thickness_limit:
-                i+=1
-                System_state,KMC_time_step = KMC(System_state,rng)
-                # quit()
-                list_time_step.append(KMC_time_step)
-                if np.mean(list_time_step[-System_state.n_search_superbasin:]) <= System_state.time_step_limits:
-                    nothing_happen +=1    
-                else:
-                    nothing_happen = 0
+            if nothing_happen >= System_state.n_search_superbasin:
+                search_superbasin(System_state)
+                nothing_happen = 0
+
+        
+            if i%snapshoots_steps== 0:
+                System_state.add_time()
                 
-                if nothing_happen >= System_state.n_search_superbasin:
-                    search_superbasin(System_state)
-                    nothing_happen = 0
+                j+=1
+                System_state.measurements_crystal()
+                # print(str(j)+"/"+str(int(total_steps/snapshoots_steps)),'| Total time: ',System_state.list_time[-1])
+                # print(str(System_state.list_time[-1]/time_limit * 100) + ' %','| Total time: ',System_state.list_time[-1])
+                print(str(System_state.thickness/thickness_limit * 100) + ' %','| Thickness: ', System_state.thickness, '| Total time: ',System_state.list_time[-1])
+                end_time = time.time()
+                if save_data:
+                    Results.measurements_crystal(System_state.list_time[-1],System_state.mass_gained,System_state.fraction_sites_occupied,
+                                                  System_state.thickness,np.mean(np.array(System_state.terraces)[np.array(System_state.terraces) > 0]),np.std(np.array(System_state.terraces)[np.array(System_state.terraces) > 0]),max(System_state.terraces),
+                                                  System_state.surf_roughness_RMS,end_time-starting_time)
     
-            
-                if i%snapshoots_steps== 0:
-                    System_state.add_time()
-                    
-                    j+=1
-                    System_state.measurements_crystal()
-                    # print(str(j)+"/"+str(int(total_steps/snapshoots_steps)),'| Total time: ',System_state.list_time[-1])
-                    # print(str(System_state.list_time[-1]/time_limit * 100) + ' %','| Total time: ',System_state.list_time[-1])
-                    print(str(System_state.thickness/thickness_limit * 100) + ' %','| Thickness: ', System_state.thickness, '| Total time: ',System_state.list_time[-1])
-                    end_time = time.time()
-                    if save_data:
-                        Results.measurements_crystal(System_state.list_time[-1],System_state.mass_gained,System_state.fraction_sites_occupied,
-                                                      System_state.thickness,np.mean(np.array(System_state.terraces)[np.array(System_state.terraces) > 0]),np.std(np.array(System_state.terraces)[np.array(System_state.terraces) > 0]),max(System_state.terraces),
-                                                      System_state.surf_roughness_RMS,end_time-starting_time)
-        
-                    System_state.plot_crystal(45,45,paths['data'],j)
-                    
-                    # print('j = ',j)
-                    # if j == 2:
-                    #     sys.exit()
-    
-    # =============================================================================
-    #     Annealing  
-    #            
-    # =============================================================================
-        elif System_state.experiment == 'annealing':
-            i = 0
-            #otal_steps = int(2.5e6)
-            total_steps = int(100)
-            System_state.measurements_crystal()
-            
-            while j*snapshoots_steps < total_steps:
-    
-                i+=1
-                System_state,KMC_time_step = KMC(System_state,rng)
+                System_state.plot_crystal(45,45,paths['data'],j)
                 
-                if i%snapshoots_steps== 0:
-                    System_state.add_time()
-                    j+=1
-                    System_state.measurements_crystal()
-                    print(str(j)+"/"+str(int(total_steps/snapshoots_steps)),'| Total time: ',System_state.list_time[-1])
-                    end_time = time.time()
-                    if save_data:
-                        Results.measurements_crystal(System_state.list_time[-1],System_state.mass_gained,System_state.fraction_sites_occupied,
-                                                      System_state.thickness,np.mean(np.array(System_state.terraces)[np.array(System_state.terraces) > 0]),np.std(np.array(System_state.terraces)[np.array(System_state.terraces) > 0]),max(System_state.terraces),
-                                                      System_state.surf_roughness_RMS,end_time-starting_time)
-                        
-                    System_state.plot_crystal(45,45,paths['data'],j)
-    
-    
-        System_state.plot_crystal(45,45)
+                # print('j = ',j)
+                # if j == 2:
+                #     sys.exit()
+
+# =============================================================================
+#     Annealing  
+#            
+# =============================================================================
+    elif System_state.experiment == 'annealing':
+        i = 0
+        #otal_steps = int(2.5e6)
+        total_steps = int(100)
+        System_state.measurements_crystal()
         
-        # Variables to save
-        variables = {'System_state' : System_state}
-        if save_data: save_variables(paths['program'],variables)
+        while j*snapshoots_steps < total_steps:
+
+            i+=1
+            System_state,KMC_time_step = KMC(System_state,rng)
+            
+            if i%snapshoots_steps== 0:
+                System_state.add_time()
+                j+=1
+                System_state.measurements_crystal()
+                print(str(j)+"/"+str(int(total_steps/snapshoots_steps)),'| Total time: ',System_state.list_time[-1])
+                end_time = time.time()
+                if save_data:
+                    Results.measurements_crystal(System_state.list_time[-1],System_state.mass_gained,System_state.fraction_sites_occupied,
+                                                  System_state.thickness,np.mean(np.array(System_state.terraces)[np.array(System_state.terraces) > 0]),np.std(np.array(System_state.terraces)[np.array(System_state.terraces) > 0]),max(System_state.terraces),
+                                                  System_state.surf_roughness_RMS,end_time-starting_time)
+                    
+                System_state.plot_crystal(45,45,paths['data'],j)
 
 
-# # Use cProfile to profile the main function
-if __name__ == '__main__':
-    cProfile.run('main()', 'profile_output.prof')    
+    System_state.plot_crystal(45,45)
+    
+    # Variables to save
+    variables = {'System_state' : System_state}
+    if save_data: save_variables(paths['program'],variables)
 
-import pstats
 
-# # Load and analyze the profiling results
-p = pstats.Stats('profile_output.prof')
-p.strip_dirs().sort_stats('time').print_stats(15)  # Show top 10 time-consuming functions
+# Use cProfile to profile the main function
+# if __name__ == '__main__':
+#     cProfile.run('main()', 'profile_output.prof')    
+
+# import pstats
+
+# Load and analyze the profiling results
+# p = pstats.Stats('profile_output.prof')
+# p.strip_dirs().sort_stats('time').print_stats(15)  # Show top 10 time-consuming functions